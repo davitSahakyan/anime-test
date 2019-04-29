18381a5c45c1d55984bd192389032b27604a46e0
ArmenianDavid <imast.un@mail.ru>
2019-04-29 17:12:07 +0400

    Updates

---
+++ precache-manifest.1cd447bcd74b2bce8e3bc2edcb74f9ec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "513dce807ffc638c9a4c964ddfe7d8e6",
+    "url": "/myfirstapp/static/media/anime5.513dce80.png"
+  },
+  {
+    "revision": "718dc0ce8c5e66cc8b8b8773964c06b2",
+    "url": "/myfirstapp/static/media/anime4.718dc0ce.png"
+  },
+  {
+    "revision": "d7f8c34e5c041075c3c7bb91e677641d",
+    "url": "/myfirstapp/static/media/anime3.d7f8c34e.jpg"
+  },
+  {
+    "revision": "2d778dc9c5a1c3f105f33eec161ca6dc",
+    "url": "/myfirstapp/static/media/anime2.2d778dc9.jpg"
+  },
+  {
+    "revision": "41dd3ac521c1ec570406c38905bfdf5b",
+    "url": "/myfirstapp/static/media/anime1.41dd3ac5.jpg"
+  },
+  {
+    "revision": "447901cdbdf6db4ec0cd",
+    "url": "/myfirstapp/static/js/runtime~main.6f409581.js"
+  },
+  {
+    "revision": "fecbbfd54bd79f77ca0b",
+    "url": "/myfirstapp/static/js/main.43b69109.chunk.js"
+  },
+  {
+    "revision": "d90af9121f69a32e1dcb",
+    "url": "/myfirstapp/static/js/2.55997850.chunk.js"
+  },
+  {
+    "revision": "fecbbfd54bd79f77ca0b",
+    "url": "/myfirstapp/static/css/main.7c81fd7d.chunk.css"
+  },
+  {
+    "revision": "3cb088481feba16b87cdae3855ad7629",
+    "url": "/myfirstapp/index.html"
+  }
+];
